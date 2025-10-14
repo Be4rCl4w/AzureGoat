2eb84bc8c12bbd83137f13b9680704be5f336b23
Be4rCl4w <121987346+Be4rCl4w@users.noreply.github.com>
2025-10-14 15:44:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,11 +25,9 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
+
+  subscription_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" # <-- Add this
 }
 
 variable "resource_group" {
